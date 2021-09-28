777764261979ac60f9205fb1cd8b2c25db235db3
shruthibss1997 <shruthibss1997@gmail.com>
2021-09-28 22:40:39 +0530

    Homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import BeforeHomePage from './pages/BeforeHomePage';
+import {Route,Switch} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/">
+          <BeforeHomePage/>
+        </Route>
+      </Switch>
     </div>
   );
 }
