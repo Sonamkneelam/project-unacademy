a5ebbd53664aaa10bd4a91e9402acc4a1c810292
N Sonam <sonamneelam037@gmail.com>
2021-10-02 18:35:42 +0530

    Linked courses to user

--- src/components/SchoolSyllabus/SchoolSyllabus.jsx
+++ src/components/SchoolSyllabus/SchoolSyllabus.jsx
@@ -4,8 +4,9 @@ import React from "react";
 import { Login } from "../Login-SignUp/Login";
 import { useState } from "react";
 import { School } from "./School";
+import axios from "axios";
 
-export const SchoolSyllabus = () => {
+export const SchoolSyllabus = ({ cat, courseId }) => {
   const [show, setShow] = useState(false);
   const [user, setUser] = useState("");
   console.log("user:", user);
@@ -14,29 +15,46 @@ export const SchoolSyllabus = () => {
     setUser(e);
   };
 
+  const handleUserDetails = async () => {
+    const { data } = await axios.get("http://localhost:3001/users");
+    const [userDetails] = data.filter((e) => e.number === user);
+    console.log("userDetails:", userDetails);
+    // console.log("data:", data);
+    const courseDetails = {
+      board: "CBSE",
+      boardId: 9,
+      category: cat,
+      courdeId: courseId,
+    };
+    axios.patch(`http://localhost:3001/users/${userDetails.id}`, courseDetails);
+  };
+
   const handleLogin = () => {
     setShow(!show);
     console.log(show);
   };
-
+  if (user) {
+    handleUserDetails();
+  }
   return (
     <div>
       <School>
-        <div className='header'>
-          <div className='navbar'>
+        <div className="header">
+          <div className="navbar">
             <img
-              src='https://static.uacdn.net/production/_next/static/images/logo.svg?q=75&w=256'
-              alt='logo'
+              src="https://static.uacdn.net/production/_next/static/images/logo.svg?q=75&w=256"
+              alt="logo"
             />
 
             {show ? (
               <>
                 <Login
                   handleLogin={handleLogin}
-                  handleUser={handleUser}></Login>
+                  handleUser={handleUser}
+                ></Login>
               </>
             ) : (
-              <button onClick={handleLogin} className='button'>
+              <button onClick={handleLogin} className="button">
                 Login
               </button>
             )}
