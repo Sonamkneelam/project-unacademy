69a3d208ec14aed9c1a3d06cd68735ef0e762681
N Sonam <sonamneelam037@gmail.com>
2021-10-02 23:43:54 +0530

    Working on CSS and added conditional rendering

--- src/components/SchoolSyllabus/SchoolSyllabus.jsx
+++ src/components/SchoolSyllabus/SchoolSyllabus.jsx
@@ -5,7 +5,8 @@ import { Login } from "../Login-SignUp/Login";
 import { useState } from "react";
 import { School } from "./School";
 import axios from "axios";
-
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 export const SchoolSyllabus = ({ cat, courseId }) => {
   const [show, setShow] = useState(false);
   const [user, setUser] = useState("");
@@ -41,25 +42,40 @@ export const SchoolSyllabus = ({ cat, courseId }) => {
       <School>
         <div className="header">
           <div className="navbar">
-            <img
-              src="https://static.uacdn.net/production/_next/static/images/logo.svg?q=75&w=256"
-              alt="logo"
-            />
-
-            {show ? (
+            <div>
+              <img
+                src="https://static.uacdn.net/production/_next/static/images/logo.svg?q=75&w=256"
+                alt="logo"
+              />
+              <div style={{ color: " #3C4852" }}>Creative Corner</div>
+            </div>
+            {user ? (
               <>
-                <Login
-                  handleLogin={handleLogin}
-                  handleUser={handleUser}
-                ></Login>
+                <div className="account">
+                  <NotificationsNoneOutlinedIcon
+                    style={{ width: "20px", height: "20px" }}
+                  ></NotificationsNoneOutlinedIcon>
+                  <AccountCircleOutlinedIcon
+                    style={{ width: "28.60px", height: "28.60px" }}
+                  ></AccountCircleOutlinedIcon>
+                </div>
               </>
             ) : (
-              <button onClick={handleLogin} className="button">
-                Login
-              </button>
+              <>
+                <button onClick={handleLogin} className="button">
+                  Login
+                </button>
+              </>
             )}
           </div>
         </div>
+        {show ? (
+          <>
+            <Login handleLogin={handleLogin} handleUser={handleUser}></Login>
+          </>
+        ) : (
+          <></>
+        )}
       </School>
     </div>
   );
